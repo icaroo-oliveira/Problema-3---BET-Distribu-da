68de10c42433d019cfdef6e25d6731ce2d8d0dfd
icaroo-oliveira <icarooo10.j@gmail.com>
2024-12-12 21:30:46 -0300

    feat: usando somente uma chave para deploy(temporario)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,4 @@
 require("@nomicfoundation/hardhat-toolbox");
-// const INFURA_API_KEY = vars.get("a94b2f6d8a1f41a7add8ef8147fc6b20");
-
-// const ETHERSCAN_API_KEY = vars.get("R8AMJAR4FNZ1H5N478DHW8NQD4CQUG7NUH");
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.27",
-//   networks: {
-//     localhost: {
-//       url: "http://127.0.0.1:8545", // Endereço padrão para o Hardhat Network ou outra rede local
-//       chainId: 31337, // Chain ID padrão para Hardhat Network
-//     },
-//   },etherscan: {
-//     apiKey: "R8AMJAR4FNZ1H5N478DHW8NQD4CQUG7NUH",
-//   },
-// };
-
-
-
 
 require('dotenv').config();
 
@@ -25,7 +7,7 @@ module.exports = {
   networks: {
     ganache: {
       url: "http://localhost:7545",
-      accounts: [process.env.PRIVATE_KEY_1, process.env.PRIVATE_KEY_2,process.env.PRIVATE_KEY_3]
+      accounts: [process.env.PRIVATE_KEY_1]
     }
   }
 };
